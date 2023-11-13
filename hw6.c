ea2f42d40a1458e0b4ea358c305ff6cd43ab097c
caosu <4siyantianji@gmail.com>
2023-11-13 16:48:13 +0800

    not finish yet

--- hw6.c
+++ hw6.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define IN_BOARD(x, y) (x >= 0 && x <= 7 && y >= 0 && y <= 7)
 #define IS_EMPTY(x) (x == 0)
 
@@ -13,26 +14,23 @@ int Direction(int board[8][8], int x, int y, int chess,int flip){
         if(IN_BOARD(tx, ty) && !IS_EMPTY(board[tx][ty])){
             int n = YesOrNo(board, chess, x, y, direc[i][0], direc[i][1]);
             sum += n;
-            if(flip){
+            if(flip && n > 0){
                 board[x][y] = chess;
+                printf("ready to flip...\n");
                 Flip(board, x, y, direc[i][0], direc[i][1], n); //wrong position
             }
         }
     }
-    
     if(!flip){   
-        _BandW[chess - 1][x * 8 + y][0] = x, 
-        _BandW[chess - 1][x * 8 + y][1] = y, 
-        // _RecordW[x * 8 + y][0] = x, 
-        // _RecordW[x * 8 + y][1] = y, 
-        _BandW[chess - 1][x * 8 + y][2] = sum;
-        if(sum > 0) return 2;
-        else return 1;
+        if(sum > 0){
+            _BandW[chess - 1][x * 8 + y][0] = x, 
+            _BandW[chess - 1][x * 8 + y][1] = y, 
+            _BandW[chess - 1][x * 8 + y][2] = sum;
+            return 1;
+        }
+        return 0;
     }
-    if(flip){
-        if(sum > 0) return 1;
-        else return 0;
-    }
+    if(flip) return sum;
 }   //Direction
 int YesOrNo(int board[8][8], int chess, int x, int y, int dx, int dy){   //找單一方向
     x = x + dx, y = y + dy;
@@ -51,36 +49,43 @@ void ClearAll(){
     }
         // _RecordW[i][0] = _RecordW[i][1] = _RecordW[i][2] = -1;
 }   //ClearAll
-void Most(int board[8][8]){
+int Most(int board[8][8]){
     int max = 0, x = 0, y = 0;
     for(int i=0; i<64; i++)
         if(_BandW[1][i][2] > max)
-            x = i / 8, y = i % 8;
-    Direction(board, x, y, 2, 1);     
+            max = _BandW[1][i][2], x = i / 8, y = i % 8;
+    printf("%d %d (n = %d)\n", x, y, max);
+    Direction(board, x, y, 2, 1); 
+    return max;    
 }   //Most
-int Move(int board[8][8],int chess){
+int CheckNext(int board[8][8],int chess){
     int flag = 0;
     for(int i=0; i<8; i++)
         for(int j=0; j<8; j++)
             if(IS_EMPTY(board[i][j]))
-                if(Direction(board, i, j, chess, 0) == 2)
+                if(Direction(board, i, j, chess, 0)){
+                    if(chess == 1) return 1;    //for black
                     flag = 1;
-    if(flag) return 1;
+                }
+    if(flag) return 1;  //for white
     return 0;
-}   //Move
+}   //CheckNext
 void Flip(int board[8][8], int x, int y,int dx, int dy, int n){
+    printf("READY dx: %d, dy: %d\n", dx, dy);
     for(int i=1; i<=n; i++){
         if(board[x + i * dx][y + i * dy] == 1)
             board[x + i * dx][y + i * dy] = 2;
         else board[x + i * dx][y + i * dy] = 1;
     }
+    printf("flipped %d chessmen\n", n);
 }   //Flip
 void PrintBoard(int board[8][8]){
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++)
             printf("%d ", board[i][j]);
         printf("\n");
-    }   
+    }  
+    // printf("\n");
 }   //PrintBoard
 int CheckBoard(int board[8][8]){
     for(int i=0; i<8; i++)
@@ -96,12 +101,13 @@ void Winner(int board[8][8]){
             if(board[i][j] == 1) sumB++;
             else if(board[i][j] == 2) sumW++;
         }
-    if(sumB > sumW) printf("Black wins!\n");
-    else if(sumB < sumW) printf("White wins!\n");
-    else printf("Tie!\n");
+    if(sumB > sumW) printf("Black wins! %d:%d", sumB, sumW);
+    else if(sumB < sumW) printf("White wins! %d:%d", sumW, sumB);
+    else printf("Tie! %d:%d", sumB, sumW);
 }   //Winner
 void main(){
-    int board[8][8] = {0}, x, y ,chess, round = 0;
+    // int board[8][8] = {0};
+    int x, y ,chess, round = 0, user, computer, play = 1;
     char first;
     int board[8][8] = {{0, 0, 2, 0, 2, 0, 2, 0},
                        {1, 0, 1, 2, 1 ,2, 0, 0},
@@ -111,30 +117,45 @@ void main(){
                        {0, 0, 1, 2, 1, 2, 0, 0},
                        {0, 0, 0, 0, 2, 0, 0, 0},                                                                                                              
                        {0, 0, 1, 1, 1, 0, 0, 0}}; //pic1
-    // for(int i=0; i<8; i++),/ 
+    // for(int i=0; i<8; i++)
     //     for(int j=0; j<8; j++)
     //         scanf("%d", &board[i][j]);
     printf("Do you want to go first?('y' or 'n'; black is first)\n");
     scanf("%c", &first);
-    if(first == 'y') round = 1; //round & 1 為0就是user time
-    while(1){
-        int flagU = 0, flagC = 0;
+    if(first == 'y') 
+        user = 1, computer = 2, round = 1;  //round & 1 為0就是user time
+    else if(first == 'n') 
+        user = 2, computer = 1;  //round & 1 為0就是user time
+    else return;
+    // !!!attention!!! 要123行到129行的話 chess要改
+    while(play){
+        int flagU = 0, flagC = 0, n;
         ClearAll(), PrintBoard(board);
         if(round & 1){
-            if(Check(board, 1)){
+            if(CheckNext(board, 1)){
                 printf("your turn: ");
-                scanf("%d%d", &x, &y);
-                if(IS_EMPTY(board[x][y]))
-                    Direction(board, x, y, 1, 1);
+                scanf("%d %d", &x, &y);
+                if(x > 7 || y > 7) break;
+                if(IS_EMPTY(board[x][y])){
+                    n = Direction(board, x, y, 1, 1);
+                    printf("U get %d chessmen\n", n);
+                }
             }
             else{
-                printf("Can't move\n");
+                printf("U can't move\n");
                 flagU = round;
             } 
         }   //user time
         else{
-            if(check(board,2)) Most(board);
-            else flagC = round;
+            if(CheckNext(board,2)){
+                printf("Computer's turn: ");
+                n = Most(board);
+                printf("Computer gets %d chessmen\n", n);   
+            } 
+            else{
+                printf("Computer can't move\n");
+                flagC = round;
+            } 
         }   //computer time
         round++;
         if(abs(flagC - flagU) == 1 || !CheckBoard(board)){
